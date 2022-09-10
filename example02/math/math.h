eed8f5ea4e8581eddf26b201c31ffbf4c2899c94
chanssuchi@gmail.com <chanssuchi@gmail.com>
2022-09-10 07:53:31 -0700

    lib.

---
+++ example02/math/math.h
@@ -0,0 +1,9 @@
+#ifndef CMAKE_EXERCISE_MATH_H
+#define CMAKE_EXERCISE_MATH_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+double div(double a, double b);
+
+#endif //CMAKE_EXERCISE_MATH_H
